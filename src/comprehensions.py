f1c20283c357456c8c8291e859581e1ed11a9ecc
Casey Bee <caseye.baker@gmail.com>
2019-04-22 15:15:38 -0700

    comprehensions.py done

--- src/comprehensions.py
+++ src/comprehensions.py
@@ -10,14 +10,14 @@ for more info regarding list comprehensions.
 
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
-y = []
+y = [i for i in range(1,6)]
 
-print (y)
+print(y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-y = []
+y = [i**3 for i in range(10)]
 
 print(y)
 
@@ -26,7 +26,7 @@ print(y)
 
 a = ["foo", "bar", "baz"]
 
-y = []
+y = [i.upper() for i in a]
 
 print(y)
 
@@ -34,8 +34,10 @@ print(y)
 # the user entered into list x.
 
 x = input("Enter comma-separated numbers: ").split(',')
+print(x)
 
 # What do you need between the square brackets to make it work?
-y = []
+#y = [for each element in the array, if % 2 == 0, append, otherwise no]
 
+y = [i for i in x if int(i)%2==0] ##split turned it into an array of strings, so change back to int to perform modulo
 print(y)
