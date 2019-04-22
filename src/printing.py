04fdc5ed8765a0789acf6715403642dea44c098f
Casey Bee <caseye.baker@gmail.com>
2019-04-22 13:13:12 -0700

    printing.py finished

--- src/printing.py
+++ src/printing.py
@@ -12,6 +12,14 @@ z = "I like turtles!"
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
 
+print('y is %f' %y) # %.2f will round to the second decimal place
+print('x is %d, y is %.2f, z is %s' %(x, y, z))
+# conversion types (%d, %s, etc) require certain datay types to work
+
 # Use the 'format' string method to print the same thing
+formatString = 'x is {}, y is {:.2f}, z is {}'
+print(formatString.format(x, y, z))
+# formatting with auto field numbering? YES. 1:.2f vs :.2f
 
 # Finally, print the same thing using an f-string
+print(f'x is {x}, y is {y:.2f}, z is {z}')
